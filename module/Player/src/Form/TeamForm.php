7f6bf4297dde4ac4514b6b85b43c68b760e5445f
Tyrell Cook <onesockrell@gmail.com>
2020-08-13 01:08:04 -0400

    save work

--- module/Player/src/Form/TeamForm.php
+++ module/Player/src/Form/TeamForm.php
@@ -43,10 +43,88 @@ class TeamForm extends Form
         ]);
 
         $coaches = new Fieldset('coaches');
+        $coachFields = ["headCoach", "headCoachType", "offCoordinator", "defCoordinator"];
+        foreach ($coachFields as $key) {
+            $newField = new Element($key);
+            $newField->setLabel($key);
+            $newField->setLabelAttributes(['class' => 'form-group col m3']);
+            $coaches->add($newField);
+//            if ($team != null && array_key_exists($key, $info)) {
+//                $newField->setValue($info[$key]);
+//            }
+        }
+        $this->add($coaches);
+
+        $personnel = [
+            "1-1",
+            "1-2",
+            "2-1",
+            "2-2",
+            "1-3",
+            "1-0",
+            "2-0",
+            "0-2",
+            "0-1",
+        ];
 
         $scheme = new Fieldset('scheme');
+        $schemeFields = [
+            "passRatio",
+            "runRatio",
+            "shotgonRatio",
+            "underCenterRatio",
+            "screenRatio",
+            "zoneRunRate",
+            "gapRunRate",
+            "playsPerGame",
+            "passPerGame",
+            "runPerGame",
+            "drivesPerGame",
+            "outsideThrows",
+            "insideThrows",
+            "throwsUnder15",
+            "throwsUnder25",
+            "throwsOver25",
+            "qbSkill",
+            "qbType",
+            "qbMobile",
+            "olPowerRun",
+            "olZoneRun",
+            "olInside",
+            "olOutside",
+            "fbRun",
+            "wr1Type",
+            "wr2Type",
+            "wr3Type",
+            "wr1Vol",
+            "wr2Vol",
+            "wr3Vol",
+            "teType",
+            "teVol",
+            "rb1Type",
+            "rb1Vol",
+            "rb2Type",
+            "rb2Vol",
+            "defRun",
+            "defPass",
+            "defPush",
+            "defRush",
+            "lbCoverage",
+            "cbCoverage",
+            "safetyCoverage"
+        ];
+        foreach ($schemeFields as $key) {
+            $newField = new Element($key);
+            $newField->setLabel($key);
+            $newField->setLabelAttributes(['class' => 'form-group col m2']);
+            $scheme->add($newField);
+//            if ($info != null && array_key_exists($key, $info)) {
+//                $newField->setValue($info[$key]);
+//            }
+        }
+        $this->add($scheme);
 
-        $support = new Fieldset('coaches');
+        $support = new Fieldset('support');
 
         $this->add([
             'name' => 'submit',
